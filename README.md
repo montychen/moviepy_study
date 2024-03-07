ffc9020a699a2e6963f639b13e4add67a099d988
montychen <dj2503@qq.com>
2024-03-07 18:09:19 +0800

    pip install moviepy

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-[moviepy](https://github.com/Zulko/moviepy) 是python的视频编辑制作工具
+[moviepy](https://github.com/Zulko/moviepy) 是python的视频编辑制作工具, 它内部会使用`ffmpeg` 、[Pillow](https://github.com/python-pillow/Pillow)
+
+### 安装 `moviepy`
+```bash
+pip install moviepy
+```
 
 [数破破](https://www.mathpo.com/)这个网站提供的动漫视频沙雕生成工具有点意思
--- study_moviepy.py
+++ study_moviepy.py
@@ -2,7 +2,7 @@ from moviepy.editor import *
 import moviepy.video.fx.all as vfx
 
 def video_clip_add_subtitle():
-    video = VideoFileClip("沙雕视频.mp4").subclip(5,20)
+    video = VideoFileClip("res/沙雕视频.mp4").subclip(5,20)
 
     # 定义中文字幕， 中文需要指定中文字体
     txt_clip = ( TextClip("Hello world! 你好世界",fontsize=70,color='white', font="font/站酷仓耳渔阳体-W01.ttf")
@@ -15,7 +15,7 @@ def video_clip_add_subtitle():
     concatenate_videoclips
 
 def overlay_img_or_video():
-    video = VideoFileClip("沙雕视频.mp4")
+    video = VideoFileClip("res/沙雕视频.mp4")
     # img = ImageClip("girl.jpg").set_duration(5).set_position((10,50)).resize(newsize=(200,300)).set_start(2)
     img = VideoFileClip("yman2.mp4").subclip(10).set_duration(5).set_position((10,50)).resize(newsize=(200,300)).set_start(2)
 
@@ -24,18 +24,18 @@ def overlay_img_or_video():
                                 
     
 def change_or_get_audio():
-    video = VideoFileClip("沙雕视频.mp4")
+    video = VideoFileClip("res/沙雕视频.mp4")
     audio_of_video = video.audio     # 提取mp4的音频
     audio_of_video.write_audiofile("output/提取mp4的音频.mp3")
 
-    new_audio = AudioFileClip("听闻远方有你_刘德华.mp3")
+    new_audio = AudioFileClip("res/听闻远方有你_刘德华.mp3")
     new_video = video.set_audio(new_audio)   # 修改视频的音频
     # new_video.volumex(2)
     new_video.write_videofile("output/new_audio.mp4")
 
 def vfx_fx():   # vfx 处理视频特效，  fx 处理音频特效
-    video = VideoFileClip("沙雕视频.mp4")
+    video = VideoFileClip("res/沙雕视频.mp4")
     color_adjust_video = video.fx(vfx.colorx, 1.5)   # 视频颜色调亮
     color_adjust_video.write_videofile("output/color_adjust.mp4")
 
-vfx_fx()
+change_or_get_audio()
